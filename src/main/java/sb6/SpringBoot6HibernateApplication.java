93123b4f03e87328b7965811cd06beb5a4efe748
Yuhyeon95 <71800820+Yuhyeon95@users.noreply.github.com>
2020-12-11 10:07:41 +0900

    Initial commit

---
+++ src/main/java/sb6/SpringBoot6HibernateApplication.java
@@ -0,0 +1,13 @@
+package sb6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot6HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot6HibernateApplication.class, args);
+	}
+
+}
